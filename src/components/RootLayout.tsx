264952bd7817e4be90ead2e7a630bc2adfe3639a
Gabriel Dias <gabrieldg0885@gmail.com>
2025-03-18 17:42:00 -0300

    fix: habilitada a importação do footer funcional no RootLayout

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header/Header";
-// import Footer from "./Footer";
+import Footer from "./Footer/Footer";
 
 const RootLayout = () => {
   return (
@@ -9,7 +9,7 @@ const RootLayout = () => {
       <main>
         <Outlet />
       </main>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 };
